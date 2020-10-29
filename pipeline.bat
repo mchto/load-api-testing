26bfde7b335045b19ca7d8c3255182045339700e
mchto <chinchi_007@hotmail.com>
2020-10-28 21:54:23 -0600

    se configura el archivo jenkins file con los stages para ejecutar los tests

--- pipeline.bat
+++ pipeline.bat
@@ -5,13 +5,21 @@ echo 'newman test'
 node --version
 cd api/newman
 newman run ExamenManuelChinchilla.postman_collection.json -e envExamManuelChinchilla.postman_environment.json
-cd ../../
+cd ..
+
+echo 'rest assured test'
+
+cd rest_assured
+mvn clean test
+cd ..
+cd ..
 
 echo 'load test'
 
 cd load
 jmeter -n -t examen_test_plan.jmx
-cd ../
+cd ..
+
 
 
 
